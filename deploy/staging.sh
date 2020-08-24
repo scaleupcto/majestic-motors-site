d43cfcf66adfce84d4df356683bde847ce232fc0
Chris Clarke <cc@talis.com>
2020-08-24 20:17:25 +0100

    Added open graph

--- deploy/staging.sh
+++ deploy/staging.sh
@@ -1,3 +1,3 @@
-JEKYLL_ENV=production bundle exec jekyll build
+JEKYLL_ENV=staging bundle exec jekyll build --config _config.yml,_config_staging.yml
 /usr/local/lib/ruby/gems/2.6.0/bin/s3_website push
 /usr/local/lib/ruby/gems/2.7.0/bin/s3_website push
