7df27dbab2906522d4ffaacdc81a7d21d699269a
Chris Clarke <cc@talis.com>
2020-08-27 14:46:07 +0100

    Little fixes

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -1,3 +1,5 @@
+export MM_S3_BUCKET=www.majestic-motors.co.uk
+export MM_CF_DIST_ID=E2V5PWG8EL9ZND
 JEKYLL_ENV=production bundle exec jekyll build
 /usr/local/lib/ruby/gems/2.6.0/bin/s3_website push
 /usr/local/lib/ruby/gems/2.7.0/bin/s3_website push
--- deploy/staging.sh
+++ deploy/staging.sh
@@ -1,3 +1,5 @@
+export MM_S3_BUCKET=staging.majestic-motors.co.uk
+export MM_CF_DIST_ID=
 JEKYLL_ENV=staging bundle exec jekyll build --config _config.yml,_config_staging.yml
 /usr/local/lib/ruby/gems/2.6.0/bin/s3_website push
 /usr/local/lib/ruby/gems/2.7.0/bin/s3_website push
